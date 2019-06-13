5660233ff1aa963a70a44a932725b36d5eae9bf5
Samuel Ramon <samuelrbo@gmail.com>
2019-06-12 23:01:30 -0300

    Include mongo module; Add mongo config;

---
+++ backend/src/api/v1/admin/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: process.env.MONGODB_SECRET,
+  database: process.env.MONGO_DB_URI,
+};
--- backend/src/api/v1/routes/auth.js
+++ backend/src/api/v1/routes/auth.js
@@ -7,6 +7,13 @@ const { AUTH_TYPES, JWT_SECRET, JWT_EXPIRATION } = require('../config/auth');
 
 module.exports = (router) => {
 
+  /**
+   * API logout
+   *
+   * @route GET /api/v1/logout
+   * @returns {object} 200
+   * @returns {Error}  default - Unexpected error
+   */
   router.get('/logout', (req, res) => {
     return res.json({ success: true, authentication: false });
   });
